fe850070291986a31cbe16655547818634b25621
pleewson <adrianplewait@gmail.com>
2024-02-15 23:53:42 +0100

    cd 30m

--- src/main/java/main/web/Servlet12.java
+++ src/main/java/main/web/Servlet12.java
@@ -1,20 +1,15 @@
 package main.web;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-@WebServlet("/servlet11")
-public class Servlet11 extends HttpServlet {
+public class Servlet12 extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        response.getWriter().append("Hello First Servlet!");
-        System.out.println("Hello First Servlet");
-        //cd
+        response.getWriter().append("Content12");
     }
 
 }
