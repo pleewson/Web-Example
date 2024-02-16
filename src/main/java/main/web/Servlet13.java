5ed77534116055f95c728839f2548b910e76c64e
pleewson <adrianplewait@gmail.com>
2024-02-16 03:04:45 +0100

    servlet that print on page time

--- src/main/java/main/web/Servlet13.java
+++ src/main/java/main/web/Servlet13.java
@@ -1,4 +1,18 @@
 package main.web;
 
-public class Servlet13 {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalTime;
+
+@WebServlet("/servlet13")
+public class Servlet13 extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append(LocalTime.now().toString());
+    }
+
+
 }
