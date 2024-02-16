5758a726b8ef9f58b19a546f7029b8adf93f3554
pleewson <adrianplewait@gmail.com>
2024-02-16 02:56:45 +0100

    added redirecting servlet

--- src/main/java/main/web/Redirect.java
+++ src/main/java/main/web/Redirect.java
@@ -7,21 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-@WebServlet("/servlet11")
-public class Servlet11 extends HttpServlet {
-
+@WebServlet ("/redirect")
+public class Redirect extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        response.getWriter().append("Hello First Servlet!");
-        System.out.println("Hello First Servlet");
+        response.sendRedirect("https://github.com/pleewson");
+        System.out.println("redirected to https://github.com/pleewson");
     }
 
-    public void init(){
-        System.out.println("init");
-    }
 
-    public void destroy(){
-        System.out.println("destroy");
-    }
 
 }
