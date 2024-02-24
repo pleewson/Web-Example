51b2c2fff306471663cdcfeb13e1bbe8491af4c1
pleewson <adrianplewait@gmail.com>
2024-02-24 23:22:37 +0100

    session -ex 5/5

---
+++ src/main/java/main/web/session/CartItem.java
@@ -0,0 +1,31 @@
+package main.web.session;
+
+public class CartItem {
+    private String name;
+    private int quantity;
+    private double price;
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
